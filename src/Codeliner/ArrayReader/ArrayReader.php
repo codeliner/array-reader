767eace13c12520d60b492f828dc8289288638b1
Codeliner <kontakt@codeliner.ws>
2014-03-08 21:43:42 +0100

    Add ArrayReader

---
+++ src/Codeliner/ArrayReader/ArrayReader.php
@@ -0,0 +1,161 @@
+<?php
+/*
+ * This file is part of the codeliner/array-reader.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 08.03.14 - 21:28
+ */
+
+namespace Codeliner\ArrayReader;
+
+/**
+ * Class ArrayReader
+ *
+ * @package Codeliner\ArrayReader
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class ArrayReader
+{
+    /**
+     * @var array
+     */
+    private $originalArray = array();
+
+    /**
+     * @param array $anArray
+     */
+    public function __construct(array $anArray)
+    {
+        $this->originalArray = $anArray;
+    }
+
+    /**
+     * @param string $aPath
+     * @param int    $default
+     * @return int
+     */
+    public function integerValue($aPath, $default = 0)
+    {
+        $value = $this->getValueFromPath($aPath);
+
+        if (is_null($value)) {
+            return intval($default);
+        }
+
+        return intval($value);
+    }
+
+    /**
+     * @param string $aPath
+     * @param float  $default
+     * @return float
+     */
+    public function floatValue($aPath, $default = 0.0)
+    {
+        $value = $this->getValueFromPath($aPath);
+
+        if (is_null($value)) {
+            return floatval($default);
+        }
+
+        return floatval($value);
+    }
+
+    /**
+     * @param string $aPath
+     * @param bool   $default
+     * @return bool
+     */
+    public function booleanValue($aPath, $default = false)
+    {
+        $value = $this->getValueFromPath($aPath);
+
+        if (is_null($value)) {
+            return boolval($default);
+        }
+
+        return boolval($value);
+    }
+
+    /**
+     * @param string $aPath
+     * @param string $default
+     * @return string
+     */
+    public function stringValue($aPath, $default = '')
+    {
+        $value = $this->getValueFromPath($aPath);
+
+        if (is_null($value)) {
+            return strval($default);
+        }
+
+        return strval($value);
+    }
+
+    /**
+     * @param string $aPath
+     * @param array  $default
+     * @return array
+     */
+    public function arrayValue($aPath, array $default = array())
+    {
+        $value = $this->getValueFromPath($aPath);
+
+        if (is_null($value)) {
+            return $default;
+        }
+
+        if (is_scalar($value)) {
+            return array($value);
+        }
+
+        if (is_object($value)) {
+            $value = json_decode(json_encode($value), true);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->originalArray;
+    }
+
+    /**
+     * @param string $aPath
+     * @return array
+     */
+    protected function toPathKeys($aPath)
+    {
+        return explode('.', $aPath);
+    }
+
+    /**
+     * @param string $aPath
+     * @return mixed
+     */
+    protected function getValueFromPath($aPath)
+    {
+        $pathKeys = $this->toPathKeys($aPath);
+
+        $arrayCopyOrValue = $this->originalArray;
+
+        foreach($pathKeys as $pathKey) {
+
+            if (!isset($arrayCopyOrValue[$pathKey])) {
+                return null;
+            }
+
+            $arrayCopyOrValue = $arrayCopyOrValue[$pathKey];
+        }
+
+        return $arrayCopyOrValue;
+    }
+}
---
+++ tests/Codeliner/ArrayReaderTest/ArrayReaderTest.php
@@ -0,0 +1,346 @@
+<?php
+/*
+ * This file is part of the codeliner/array-reader.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 08.03.14 - 21:30
+ */
+namespace Codeliner\ArrayReaderTest;
+
+use Codeliner\ArrayReader\ArrayReader;
+
+/**
+ * Class ArrayReaderTest
+ *
+ * @package Codeliner\ArrayReaderTest
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class ArrayReaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function is_integer_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 1
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(1, $arrayReader->integerValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_string_converted_to_integer_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => '1'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(1, $arrayReader->integerValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_default_integer_returned_when_path_not_exists()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => '1'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(2, $arrayReader->integerValue('hash.with.unknown', 2));
+    }
+
+    /**
+     * @test
+     */
+    public function is_float_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 1.0
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(1.0, $arrayReader->floatValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_string_converted_to_float_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => '1.0'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(1.0, $arrayReader->floatValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_default_float_returned_when_path_not_exists()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => '1.0'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(2.1, $arrayReader->floatValue('hash.with.unknown', 2.1));
+    }
+
+    /**
+     * @test
+     */
+    public function is_boolean_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => true
+                    )
+                )
+            )
+        );
+
+        $this->assertTrue($arrayReader->booleanValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_string_converted_to_boolean_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => '1'
+                    )
+                )
+            )
+        );
+
+        $this->assertTrue($arrayReader->booleanValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_default_boolean_returned_when_path_not_exists()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => true
+                    )
+                )
+            )
+        );
+
+        $this->assertFalse($arrayReader->booleanValue('hash.with.unknown'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_string_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 'value'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame('value', $arrayReader->stringValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_float_converted_to_string_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 1.1
+                    )
+                )
+            )
+        );
+
+        $this->assertSame('1.1', $arrayReader->stringValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_default_string_returned_when_path_not_exists()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 'value'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame('default', $arrayReader->stringValue('hash.with.unknown', 'default'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_array_value()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => array(
+                            'value'
+                        )
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(array('value'), $arrayReader->arrayValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_object_converted_to_array_value()
+    {
+        $object = new \stdClass();
+        $object->param = 'value';
+
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => $object
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(array('param' => 'value'), $arrayReader->arrayValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_scalar_converted_to_array_value()
+    {
+        $object = new \stdClass();
+        $object->param = 'value';
+
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 'scalar value'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(array('scalar value'), $arrayReader->arrayValue('hash.with.nested'));
+    }
+
+    /**
+     * @test
+     */
+    public function is_default_array_returned_when_path_not_exists()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 'value'
+                    )
+                )
+            )
+        );
+
+        $this->assertSame(array('default'), $arrayReader->arrayValue('hash.with.unknown', array('default')));
+    }
+
+    /**
+     * @test
+     */
+    public function is_original_array_returned()
+    {
+        $arrayReader = new ArrayReader(
+            array(
+                'hash' => array(
+                    'with' => array(
+                        'nested' => 'value'
+                    )
+                )
+            )
+        );
+
+        $check = array(
+            'hash' => array(
+                'with' => array(
+                    'nested' => 'value'
+                )
+            )
+        );
+
+        $this->assertEquals($check, $arrayReader->toArray());
+    }
+}
+
+ 
